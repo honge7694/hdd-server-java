bd02ab1f23f09df2ea9363eee50241967192612f
hongui <honge7694@naver.com>
2025-06-03 01:55:02 +0900

    전역 ApiResponse 추가

---
+++ src/main/java/kr/hhplus/be/server/global/response/ApiResponse.java
@@ -0,0 +1,21 @@
+package kr.hhplus.be.server.global.response;
+
+public class ApiResponse<T> {
+    private int status;
+    private String message;
+    private T data;
+
+    private ApiResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<T>(200, "success", data);
+    }
+
+    public static <T> ApiResponse<T> error(int status, String message) {
+        return new ApiResponse<T>(status, message, null);
+    }
+}
