168a561d9453dd42d68e5a346855526a5886066a
hongui <honge7694@naver.com>
2025-06-03 16:40:38 +0900

    swagger 문서 버전 수정 및 application.yml 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	// swagger
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testCompileOnly("org.projectlombok:lombok")
+	testAnnotationProcessor("org.projectlombok:lombok")
 
 	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
