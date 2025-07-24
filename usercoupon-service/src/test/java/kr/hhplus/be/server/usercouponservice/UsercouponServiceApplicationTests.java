4ebdf2ee85f94e2a86c2808072c6a9766c80a497
hongui <honge7694@naver.com>
2025-07-25 00:21:17 +0900

    feat: usercoupon-service 누락 추가

---
+++ usercoupon-service/src/test/java/kr/hhplus/be/server/usercouponservice/UsercouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hhplus.be.server.usercouponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercouponServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
